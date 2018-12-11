98812db210bb2576c11add8f657548c769d6345d
bahieu123123 <ba.hieu0207@gmail.com>
2018-12-11 15:10:16 +0300

    7/12

--- src/Test/Test/BloomFilterTest.java
+++ src/Test/Test/BloomFilterTest.java
@@ -1,58 +1,62 @@
 package Test;
 
 import Main.BloomFilter;
+import org.junit.jupiter.api.Test;
 
-import java.util.Scanner;
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertFalse;
+import static junit.framework.TestCase.assertTrue;
+
 
 public class BloomFilterTest
 {
-    public static void main(String[] args)
-    {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Bloom Filter Test\n");
-
-        System.out.println("Enter set capacity and key size");
-        BloomFilter bf = new BloomFilter(scan.nextInt() , scan.nextInt());
-        char ch;
-        /*  Perform bloom filter operations  */
-        do
-        {
-            System.out.println("\nBloomFilter Operations\n");
-            System.out.println("1. add ");
-            System.out.println("2. check");
-            System.out.println("3. check empty");
-            System.out.println("4. clear");
-            System.out.println("5. size");
-
-            int choice = scan.nextInt();
-            switch (choice)
-            {
-                case 1 :
-                    System.out.println("Enter integer element to insert");
-                    bf.add( new Integer(scan.nextInt()) );
-                    break;
-                case 2 :
-                    System.out.println("Enter integer element to search");
-                    System.out.println("Search result : "+ bf.check(new Integer(scan.nextInt()) ));
-                    break;
-                case 3 :
-                    System.out.println("Empty status = "+ bf.isEmpty());
-                    break;
-                case 4 :
-                    System.out.println("\nBloom set Cleared");
-                    bf.makeEmpty();
-                    break;
-                case 5 :
-                    System.out.println("\nSize = "+ bf.getSize() );
-                    break;
-                default :
-                    System.out.println("Wrong Entry \n ");
-                    break;
-            }
-
-            System.out.println("\nDo you want to continue (Type y or n) \n");
-            ch = scan.next().charAt(0);
-        } while (ch == 'Y'|| ch == 'y');
+    int capacity=100;
+    int k=3;
+    BloomFilter bloomFilter1=new BloomFilter(capacity,k);
+    @Test
+    public void makeEmptyTest(){
+        bloomFilter1.add(1);
+        bloomFilter1.add(2);
+        bloomFilter1.add(3);
+        bloomFilter1.add(4);
+        bloomFilter1.add(5);
+        assertFalse(bloomFilter1.isEmpty());
+        bloomFilter1.makeEmpty();
+        assertTrue(bloomFilter1.isEmpty());
     }
+    @Test
+    public void getSizeTest(){
+        for (int i=0;i<20;i++){
+            bloomFilter1.add(i);
+        }
+        assertEquals(20,bloomFilter1.getSize());
+        bloomFilter1.add(50);
+        assertEquals(21,bloomFilter1.getSize());
+        bloomFilter1.makeEmpty();
+        assertEquals(0,bloomFilter1.getSize());
+    }
+    @Test
+    public void addTest(){
+        for (int i=10;i<20;i++){
+            bloomFilter1.add(i);
+        }
+        assertTrue(bloomFilter1.check(10));
+        assertTrue(bloomFilter1.check(15));
+        assertFalse(bloomFilter1.check(50));
+        assertFalse(bloomFilter1.check(3));
+
+    }
+    @Test
+    public void checkTest(){
+        bloomFilter1.add(0);
+        bloomFilter1.add(2);
+        bloomFilter1.add(7);
+        assertTrue(bloomFilter1.check(0));
+        assertTrue(bloomFilter1.check(7));
+        assertFalse(bloomFilter1.check(10));
+        assertFalse(bloomFilter1.check(15));
+    }
+
+
 }
 
